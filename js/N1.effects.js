ecd4251b4b62a38bdffec7dbb5a9ab3837ade955
datagutt <datagutt666@hotmail.com>
2011-12-15 14:17:50 +0100

    No need to use selector for getOpacity anymore, use  instead.

--- js/N1.effects.js
+++ js/N1.effects.js
@@ -3,7 +3,7 @@
 	if(N1 && N1.isHostMethod && N1.isHostMethod(N1, 'extend') && N1.isHostMethod(N1, 'setOpacity')){
 		N1.extend({effects:{
 			fadeIn: function(selector, duration){
-				var $el = N1.getElement(selector), timer, i = N1.getOpacity(selector);
+				var $el = N1.getElement(selector), timer, i = N1.getOpacity($el);
 				
 				if(isNaN(duration)){
 					duration = 1000;
@@ -12,19 +12,19 @@
 					clearInterval($el._timer);	/*If some animation is set, clear it*/
 					var step = ( duration / ( ( 1 - i ) * 100 ) );	/*Calculate time of step*/
 					$el._timer = setInterval(function(){
-						i = N1.getOpacity(selector);
+						i = N1.getOpacity($el);
 						if(i >= 1){
 							return clearInterval($el._timer)
 						}
-						N1.setOpacity(selector, i + 0.01);
+						N1.setOpacity($el, i + 0.01);
 					}, step);
 				}catch(e){
-					N1.removeStyle(selector, "display");
+					N1.removeStyle($el, "display");
 				}
 				return $el;
 			},
 			fadeOut: function(selector, duration){
-				var $el = N1.getElement(selector), timer, i = N1.getOpacity(selector);
+				var $el = N1.getElement(selector), timer, i = N1.getOpacity($el);
 				
 				if(isNaN(duration)){
 					duration = 1000;
@@ -34,14 +34,14 @@
 					var step = ( duration / ( i * 100 ) );	/*Calculate time of step*/
 					
 					$el._timer = setInterval(function(){
-						i = N1.getOpacity(selector);
+						i = N1.getOpacity($el);
 						if(i <= 0){
 							return clearInterval($el._timer);
 						} 
-						N1.setOpacity(selector, i - 0.01);
+						N1.setOpacity($el, i - 0.01);
 					}, step);
 				}catch(e){
-					N1.setStyle(selector, { display: "none" });
+					N1.setStyle($el, { display: "none" });
 				}
 				return $el;
 			}
